f8eae52e688a231fd7792a23b3a0863070802703
Robin Tom <robin.tom.73@gmail.com>
2018-11-17 10:25:26 +0530

    Pipe implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GokartListComponent } from './gokart-list/gokart-list.component';
+import { ConvertToSpacesPipe } from './gokart-list/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     GokartListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
